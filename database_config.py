72ece4084068cef35d80bde9245eb2eb056d8772
Majid Shajari <majidpy89@gmail.com>
2023-06-22 22:43:02 +0330

    Update pyproject for replit

--- database_config.py
+++ database_config.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 from configparser import ConfigParser
+
 import psycopg2
-from psycopg2.extensions import parse_dsn
 import pymongo
+from psycopg2.extensions import parse_dsn
 
 
 class DatabaseConfig:
@@ -20,8 +21,7 @@ class DatabaseConfig:
             for param in params:
                 db_params[param[0]] = param[1]
         else:
-            raise Exception(
-                f'Section {section} not found in the {self.filename} file')
+            raise Exception(f"Section {section} not found in the {self.filename} file")
         return db_params
 
     def _check_connect_mongodb(self, url: str):
@@ -30,12 +30,11 @@ class DatabaseConfig:
         client = None
         try:
             # get a handle to the database
-            print('Connecting to the MongoDB database...')
+            print("Connecting to the MongoDB database...")
             # set a 5-second connection timeout
-            client = pymongo.MongoClient(
-                url, serverSelectionTimeoutMS=5000)
+            client = pymongo.MongoClient(url, serverSelectionTimeoutMS=5000)
             print("MongoDB version:")
-            print(client.server_info()['version'])
+            print(client.server_info()["version"])
             print("MongoDB Connection closed.")
             client.close()
             return True
@@ -43,54 +42,56 @@ class DatabaseConfig:
             print("Unable to connect to the MongoDB server.")
 
     def _check_connect_postgresql(self, url: str):
-        """ Connect to the PostgreSQL database server """
+        """Connect to the PostgreSQL database server"""
         conn = None
         try:
             # read connection parameters
             params = parse_dsn(url)
             # connect to the PostgreSQL server
-            print('Connecting to the PostgreSQL database...')
+            print("Connecting to the PostgreSQL database...")
             conn = psycopg2.connect(**params)
             # create a cursor
             cur = conn.cursor()
             # execute a statement
-            print('PostgreSQL database version:')
-            cur.execute('SELECT version()')
-
+            print("PostgreSQL database version:")
+            cur.execute("SELECT version()")
             # display the PostgreSQL database server version
             db_version = cur.fetchone()
             print(db_version)
-
             # close the communication with the PostgreSQL
             cur.close()
             conn.close()
-            print('PostgreSQL connection closed.')
+            print("PostgreSQL connection closed.")
             return True
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
     @property
     def mongodb_url(self):
-        params = self._get_params(section='mongodb')
-        conn_url = "mongodb://" +\
-            f"{params['username']}:{params['password']}" + \
-            f"@{params['host']}:{params['port']}" +\
-            "/?authMechanism=DEFAULT"
+        params = self._get_params(section="mongodb")
+        conn_url = (
+            "mongodb://"
+            + f"{params['username']}:{params['password']}"
+            + f"@{params['host']}:{params['port']}"
+            + "/?authMechanism=DEFAULT"
+        )
         return conn_url
 
     @property
     def postgresql_url(self):
-        params = self._get_params(section='postgresql')
-        conn_url = "postgresql://" +\
-            f"{params['user']}:{params['password']}" +\
-            f"@{params['host']}: {params['port']}" +\
-            f"/{params['dbname']}"
+        params = self._get_params(section="postgresql")
+        conn_url = (
+            "postgresql://"
+            + f"{params['user']}:{params['password']}"
+            + f"@{params['host']}: {params['port']}"
+            + f"/{params['dbname']}"
+        )
         if self._check_connect_postgresql(conn_url):
             return conn_url
         return None
 
 
 if __name__ == "__main__":
-    db_config = DatabaseConfig('database.ini')
-    db_config.postgresql_url
-    db_config.mongodb_url
+    import os
+
+    print(os.getenv("user"))
