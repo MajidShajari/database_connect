e1c7f7c0bffdf06d54225dc67f0adcafd08d72b9
Majid Shajari <majidpy89@gmail.com>
2023-06-22 20:57:20 +0330

    Update connect_mongodb and remove main

--- connect_mongodb.py
+++ connect_mongodb.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 import pymongo
+
 from config import config_mongodb
+
 # Replace the uri string with your MongoDB deployment's connection string.
 params = config_mongodb()
-CONN_STR = "mongodb://" +\
-    f"{params['username']}:{params['password']}" + \
-    f"@{params['host']}:{params['port']}" +\
-    "/?authMechanism=DEFAULT"
+CONN_STR = (
+    "mongodb://"
+    + f"{params['username']}:{params['password']}"
+    + f"@{params['host']}:{params['port']}"
+    + "/?authMechanism=DEFAULT"
+)
 # set a 5-second connection timeout
 client = pymongo.MongoClient(CONN_STR, serverSelectionTimeoutMS=5000)
 try:
     print(client.list_database_names())
-except Exception:
-    print("Unable to connect to the server.")
+except Exception as error:
+    print("Unable to connect to the server.\n", error)
--- main.py
+++
@@ -1,5 +0,0 @@
-from persiantools import characters
-from persiantools.jdatetime import JalaliDate
-
-
-print(characters.fa_to_ar("حبی"))
